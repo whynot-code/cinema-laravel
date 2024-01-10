eb4a90527c53fef4414d851e0e1279bd724a13ac
Patryk Seroka <whynot.dotslave@gmail.com>
2024-01-10 22:04:40 +0100

    Delete unconfirmed reservation mechanism added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\Reservation;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,24 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        //Gdzie zadeklarować poniższą funkcję?     <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 
+        $schedule->call(function () {
+            $reservations = Reservation::all();
+            foreach ($reservations as $reservation)
+            {
+                if($reservation->repertoire->display_date === now()->toDateString())
+                {
+                    $display_time = explode(":", $reservation->repertoire->display_time);
+                    $display_time_in_seconds = ($display_time[0]-1) * 3600 + $display_time[1] * 60 + $display_time[2];
+                    $delete_time = now('Europe/Berlin')->format('H') * 3600 + now()->format('i') * 60 + now()->format('s');
+
+                    if ($display_time_in_seconds <= $delete_time) 
+                    {
+                        Reservation::where('uuid', $reservation->uuid)->delete();
+                    }
+                }
+            }
+        })->everySecond();
     }
 
     /**
